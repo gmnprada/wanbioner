1813cac7c78abc4728bc8281c412727591437ad3
Shri Brahmana <adhenovan@gmail.com>
2023-02-15 08:22:01 +0800

    oh thats is

--- piwan-js/index.mjs
+++ piwan-js/index.mjs
@@ -167,8 +167,8 @@ if (os.hostname() == "piwan.net") {
     };
     var httpsServer = https.createServer(credentials, app);
     httpsServer.listen(json.PWAN_HTTPS_PORT);
-    const wss = new WebSocketServer({ "server": httpsServer });
-    
+    const wss = new WebSocketServer({ noServer: true });
+
     httpsServer.on('upgrade', (request, socket, head) => {
         wss.handleUpgrade(request, socket, head, socket => {
             wss.emit('connection', socket, request);
