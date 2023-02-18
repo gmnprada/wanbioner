33c47c914609ab1c5c1affbb6a71141e2245e886
Shri Brahmana <adhenovan@gmail.com>
2023-02-18 22:41:27 +0800

    try sync

--- piwan-js/routes/privacy.mjs
+++ piwan-js/routes/privacy.mjs
@@ -1,9 +1,9 @@
-import { readFile } from "node:fs";
+import { readFileSync } from "node:fs";
 import { ROOT_DIR } from "../pathHelper.mjs";
 
 export async function RoutePrivacy(req,res){
     let header = `{{> header title="Π Wide Area Network - Terms Of Service"}}`;
-    let docs = await readFile(ROOT_DIR + "/views/gen/privacy.html");
+    let docs = await readFileSync(ROOT_DIR + "/views/gen/privacy.html");
     let footer = `{{> footer}}`;
     let notice = `\n You Can Get Original Txt copy of this file at <a href="https://${process.env.PIWAN_DOMAIN}/assets/txt/privacy.txt">`;
 
