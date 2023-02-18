72f5716a4d3bb2086c08a8a4baca5e81e1186c82
Shri Brahmana <adhenovan@gmail.com>
2023-02-18 22:46:49 +0800

    add auto gen tos

--- piwan-js/routes/tos.mjs
+++ piwan-js/routes/tos.mjs
@@ -1,12 +1,15 @@
-import { readFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 import { ROOT_DIR } from "../pathHelper.mjs";
 
 export async function RouteTos(req,res){
+    let directory = ROOT_DIR + "/views/gen/tos.html";
     let header = `{{> header title="Π Wide Area Network - Terms Of Service"}}`;
-    let docs = await readFileSync(ROOT_DIR + "/views/gen/tos.html");
+    let docs = readFileSync(directory);
     let footer = `{{> footer}}`;
     let notice = `\n You Can Get Original Txt copy of this file at <a href="https://${process.env.PIWAN_DOMAIN}/assets/txt/tos.txt">`;
 
     let complete = header + docs + footer + notice;
-    return res.render(complete);
+
+    writeFileSync(ROOT_DIR + "/views/gen/tos_auto.html",complete);
+    return res.render('gen/tos_auto.html');
 }
