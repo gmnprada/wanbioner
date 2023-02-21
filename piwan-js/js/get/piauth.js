9b4860fad6e88f532a02a14a7bedfc1ed78ed6fe
Shri Brahmana <adhenovan@gmail.com>
2023-02-22 05:11:27 +0800

    try basic auth

--- piwan-js/js/get/piauth.js
+++ piwan-js/js/get/piauth.js
@@ -1,20 +1,26 @@
 'use strict';
 
 (async()=>{
-    const scopes = ['payments'];
+
+    const INFO = document.querySelector('#info');
+    const scopes = ['username','payments','wallet_address'];
 
     function onIncompletePayment(payment){
         console.log("Auth User",payment);
     }
 
     async function Auth(){
-        return Pi.authenticate(scopes,onIncompletePayment);
+        return await Pi.authenticate(scopes,onIncompletePayment);
     }
 
     if(Pi){
-        console.log("Pi Sdk Available Yay");
-        Pi.init({ version: "2.0",sandbox:true});
-
-        const user = await Auth();
+        //console.log("Pi Sdk Available Yay");
+        try{
+            Pi.init({ version: "2.0",sandbox:true});
+            const user = await Auth();
+            INFO.innerHTML(user);
+        }catch(e){
+            INFO.innerHTML(e);
+        }
     }
 })();
