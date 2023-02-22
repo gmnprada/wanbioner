bf0a27b3ccfacc1973bc20713755e703d997009e
Shri Brahmana <adhenovan@gmail.com>
2023-02-22 12:20:19 +0800

    set no cache

--- piwan-js/index.mjs
+++ piwan-js/index.mjs
@@ -95,6 +95,7 @@ app.use( (req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader("X-Frame-Options", 'SAMEORIGIN');
+    res.set('Cache-control','no-cache');
     next();
 });
 
