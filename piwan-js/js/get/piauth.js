260b874302832c563399920786a6496151423a68
Shri Brahmana <adhenovan@gmail.com>
2023-02-22 13:41:49 +0800

    remove sandbox mode

--- piwan-js/js/get/piauth.js
+++ piwan-js/js/get/piauth.js
@@ -1,38 +1,43 @@
-var isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
-async function init(){
+var isBrowser = new Function("try {return this===window;}catch(e){ return false;}");
+async function init() {
 
-    if(!isBrowser()) throw Error("This is not browser environment");
+    if (!isBrowser()) throw Error("This is not browser environment");
 
-    window.addEventListener('DOMContentLoaded',async (evt)=>{
+    async function Auth() {
+        try {
+            const scopes = ['username', 'payments'];
+            let data = await window.Pi.authenticate(scopes, onIncompletePayment);
+            return data;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function onIncompletePayment(payment) {
+        console.log("Auth User", payment);
+    }
+
+    window.addEventListener('DOMContentLoaded', async (evt) => {
         const INFO = document.querySelector('#info');
-        const scopes = ['username','payments'];
+        const btn = document.querySelector('#piauth');
+        btn.addEventListener('click', Auth);
         console.log("Document Fully Loaded");
-        try{
+        try {
             const Pi = window.Pi;
-            let LoadPi = await Pi.init({ version: "2.0",sandbox:false});
-            let User = await Auth();
-            INFO.innerHTML(user);
+            await Pi.init({ version: "2.0", sandbox: false });
             console.log(User);
-        }catch(e){
+        } catch (e) {
             INFO.innerHTML(e);
         }
+
+
     });
 
-    function onIncompletePayment(payment){
-        console.log("Auth User",payment);
-    }
 
-    async function Auth(){
-        try{
-            return await Pi.authenticate(scopes,onIncompletePayment);
-        }catch(e){
-            console.error(e);
-        }
-    }
 };
 
-try{
+try {
     init();
-}catch(e){
+} catch (e) {
     console.error(e);
 }
