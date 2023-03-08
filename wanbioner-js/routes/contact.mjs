dbf5f8fe030d9f516666b0ba19dd6b7c09784ffe
Gde Made Novan Priambhada <madepriambhada@gmail.com>
2023-03-09 01:11:21 +0800

    use capital LETTER for important things, add some docs and map

---
+++ wanbioner-js/routes/contact.mjs
@@ -0,0 +1,24 @@
+/*
+***** THIS FILE IS PART OF Wanbioner Project *****
+
+PiOS License Copyright (C) <2023> <gdemadenovanpriambhada>
+
+Developer
+
+
+Contributor
+
+
+See The LICENSE DETAILS of the PROJECT Under PiOS license on the root directory
+if you cannot find the copy of full license on the root project see it at
+https://github.com/pi-apps/PiOS
+or
+https://github.com/madepriambhada/piwan/blob/main/LICENSE
+
+©2023 πwan Developer And Contributor | All Rights Reserved
+
+*/
+
+export async function RouteCommunity(req,res){
+    return res.render("get/community.html");
+}
