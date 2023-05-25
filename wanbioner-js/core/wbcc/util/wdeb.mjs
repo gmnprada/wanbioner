939510a22fdc6aa68b14e23697bdf9426154ae3d
GMNPRADA <madepriambhada@gmail.com>
2023-05-25 12:15:41 -0400

    focused and while other taking care my phone case

--- wanbioner-js/core/wbcc/util/wdeb.mjs
+++ wanbioner-js/core/wbcc/util/wdeb.mjs
@@ -22,3 +22,54 @@ Focus
 -- have a manner and study ethical hacking if you hacking and use this tools as it can be used to reverse engineer binary , compiled program , or propertiary things
 -- abuse and exploit of usage on this program should be reported to MAINTAINER File if it violate trademark , policy , or propertiary things of individual or company
 */
+
+import { format_as_u128, format_as_u16, format_as_u64 } from "./wform.mjs";
+import { wlog_debug, wlog_emergency } from "./wlog.mjs";
+
+// any input data
+export function wdeb(data){
+
+    let javascriptType = [
+        "undefined",
+        "object",
+        "boolean",
+        "number",
+        "bigint",
+        "string",
+        "symbol",
+        "function",
+        "null" // also treated as object from mozilla refrences or 0 on 2016 archive
+    ]
+
+    let dataType = typeof data;
+
+    // Data type is on standard
+    if(javascriptType.includes(dataType)){
+        wlog_debug("On Standard Common Data Type");
+    } else{
+        wlog_emergency("Uknown Data Type");
+    }
+
+    if(dataType === "number"){
+        // will be exit process to determined its strict type if fail
+        let u8 = format_as_u8(data);
+        let u16 = format_as_u16(data);
+        let u32 = format_as_u32(data);
+        let u64 = format_as_u64(data);
+        let u128 = format_as_u128(data);
+        let i8  = format_as_i8(data);
+        let i32 = format_as_i32(data);
+        let i64 = format_as_i64(data);
+        let i128 = format_as_i128(data);
+    }
+
+    if(dataType === "string"){
+        // will be print output in various encoding format
+
+
+    }
+
+    if(dataType === 'boolean'){
+        
+    }
+}
